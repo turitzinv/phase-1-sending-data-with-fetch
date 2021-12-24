828a6d44c03631d18e2bf06e0ced51be3904a4a8
Victor Turitzin <turitzinv@gmail.com>
2021-12-23 21:47:37 -0500

    commit tree e6235b84197dd39164e8fd06d339b49d8c8f2432

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+/*function submitData(name, email) {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  Accept: "application/json",
+  },
+  return fetch('http://localhost:3000/users')
+  body: JSON.stringify({name,email})
+} */
+
+function submitData(name, email){
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({name,email}),
+  })
+  .then(response => response.json())
+  .then(data => document.querySelector("body").append(data.id))
+  .catch(message => document.body.append(message))
+}
+
